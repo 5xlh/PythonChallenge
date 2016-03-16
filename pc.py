6426e2b8616031a69a9770d23777bfc2b91b4fd6
5xlh <marwayhs@hotmail.com>
2016-03-15 21:19:38 -0400

    working level 3
    off to level 3

--- pc.py
+++ pc.py
@@ -1,8 +1,16 @@
-alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-beta=alpha[1:]
-words=list("g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr\'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.")
-for x in words:
-   x=
-   
-print ("".join(words))
-
+####alpha=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+####beta=alpha[2:]
+####gamma=beta+alpha[:2]
+####str(gamma).strip('[]')
+####a="".join(gamma)
+####str(alpha).strip('[]')
+####b="".join(alpha)
+##
+##roll="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyrq ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj.";
+####print (str.maketrans(a,b))
+##a="abcdefghijklmnopqrstuvwxyz"
+##b="cdefghijklmnopqrstuvwxyzab"
+##trantab=str.maketrans(a,b)
+##print (roll.translate(trantab))
+##
+##print("map".translate(trantab))
